2e241b3b780e1235d9caa2281874554973746e5d
Simon Roberts <simonroberts0204@outlook.com>
2022-08-03 12:17:31 -0400

    custom window aggregator

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from utils import setup_web3_connection, to_keccak, get_sum_over_filter, ETH_PRE
 import time
 
 
-def main(contract_address, event_sig, event_parser, sum_parser, window):
+def main(contract_address, event_sig, event_parser, aggregate_parser, aggregator, window):
 
-    print(f"Sum of {event_sig} events on {contract_address[:10]}..., window = {window}")
+    print(f"Aggregating {event_sig} events on {contract_address[:10]}... over a {window}s window")
     print("Listening...")
 
     # Instantiate a web3 connection
@@ -16,8 +16,8 @@ def main(contract_address, event_sig, event_parser, sum_parser, window):
     # Filter to listen for events
     filt = w3.eth.filter(
         {
-            "fromBlock": 'latest',
-            "toBlock": 'latest',
+            "fromBlock": "latest",
+            "toBlock": "latest",
             "address": contract_address,
             "topics": [to_keccak(event_sig)],
         }
@@ -35,8 +35,10 @@ def main(contract_address, event_sig, event_parser, sum_parser, window):
             # Discard sums that happened before the current window
             sums = {k: v for k,v in sums.items() if k > block_timestamp - window}
 
-            window_sum = sum(sums.values())
-            sum_parser(window_sum)  
+            # If dict is not empty, aggregate the sums over the window and parse the result
+            if sums:
+                window_agg = aggregator(sums.values())
+                aggregate_parser(window_agg)  
 
         time.sleep(1)
 
@@ -46,36 +48,32 @@ if __name__ == "__main__":
 
     # Configure script with the following:
 
-    # The address and event signature to listen to
-    CONTRACT_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
-    EVENT_SIG = "Transfer(address,address,uint256)"
-
-
     # A parser for extracting values from the specified event
     # For this example, DAI transfer is `Transfer(address indexed src, address indexed dst, uint wad)`
     # So `amount` is in the data
     def parse_dai_transfer(event):
-        return int(event['data'], 16)
+        return int(event["data"], 16)
 
 
-    # A parser for the window sum
-    # e.g.
-    # if window_sum > threshold:
-    #   do_something()
-    # For this example, just print the sum if > 0
-    def parse_dai_sum(window_sum):
-        if window_sum > 0:
-            print(f"{window_sum / ETH_PRECISION :.0f}")
+    # A parser for the window aggregate
+    # For this example, just print the aggregate if > 0
+    def parse_dai_aggregate(window_agg):
+        if window_agg > 0:
+            print(f"{window_agg / ETH_PRECISION :.0f}")
 
 
-    # The length of the rolling window to sum event values over
+    # The length of the rolling window to aggregate event values over
     window = 60
 
+    # The aggregator function to use over the window
+    # (E.g. sum, mean, custom function, etc.)
+    aggregator = sum
 
     main(
-        contract_address=CONTRACT_ADDRESS,
-        event_sig = EVENT_SIG,
-        event_parser=parse_dai_transfer,
-        sum_parser = parse_dai_sum,
-        window=60,
+        contract_address = "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+        event_sig = "Transfer(address,address,uint256)",
+        event_parser = parse_dai_transfer,
+        aggregate_parser = parse_dai_aggregate,
+        aggregator = aggregator,
+        window = window,
         )
